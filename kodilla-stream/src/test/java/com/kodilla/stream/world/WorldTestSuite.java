0fdea62c033b91a16e73a43609d3aab70dcb5b57
zieluzieluzielu <mac.ziel@gmail.com>
2019-10-28 20:51:41 +0100

    Module 7.6

--- kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -11,20 +11,20 @@ public class WorldTestSuite {
     public void testGetPeopleQuantity() {
 
         //GIVEN
-        Continent Europe = new Continent();
-        Europe.addCountry(new Country("Polska", new BigDecimal("23412341234")));
-        Europe.addCountry(new Country("Italy",new BigDecimal("21312321424")));
-        Europe.addCountry(new Country("Germany", new BigDecimal("123241554")));
+        Continent europe = new Continent();
+        europe.addCountry(new Country("Polska", new BigDecimal("23412341234")));
+        europe.addCountry(new Country("Italy",new BigDecimal("21312321424")));
+        europe.addCountry(new Country("Germany", new BigDecimal("123241554")));
 
-        Continent Africa = new Continent();
-        Africa.addCountry(new Country("Congo", new BigDecimal("555555435")));
-        Africa.addCountry(new Country("Egypt",new BigDecimal("2313213155")));
-        Africa.addCountry(new Country("Nigeria", new BigDecimal("13213311")));
+        Continent africa = new Continent();
+        africa.addCountry(new Country("Congo", new BigDecimal("555555435")));
+        africa.addCountry(new Country("Egypt",new BigDecimal("2313213155")));
+        africa.addCountry(new Country("Nigeria", new BigDecimal("13213311")));
 
         //WHEN
         World world = new World();
-        world.addContinent(Africa);
-        world.addContinent(Europe);
+        world.addContinent(africa);
+        world.addContinent(europe);
         BigDecimal actual = world.getPeopleQuantity();
 
         //THEN
