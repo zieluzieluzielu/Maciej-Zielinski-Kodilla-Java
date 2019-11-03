e1e89cba2d9f6454377abba2ece0efb33f1f47cd
zieluzieluzielu <mac.ziel@gmail.com>
2019-11-03 17:36:25 +0100

    Module 8.2

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -2,38 +2,28 @@ package com.kodilla.exception.io;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.util.Enumeration;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile() throws IOException {
+    public void readFile() {
+
         ClassLoader classLoader = getClass().getClassLoader();
-        Enumeration<URL> resources = classLoader.getResources("");
-        while (resources.hasMoreElements()) {
-            URL url = resources.nextElement();
-            System.out.println(url);
-        }
         File file = new File(classLoader.getResource("file/names.txt").getFile());
+        System.out.println(file.getPath());
 
-
-
-      /*  ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("file/names.txt").getFile());
-        // System.out.println(file.getPath());
-        // Path path = Paths.get(file.getPath());
-        // Path path = Paths.get("file/nofile.txt");
-
-        try(Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
             fileLines.forEach(System.out::println);
         } catch (IOException e) {
             System.out.println("something is wrong! Error: " + e);
-        }
-        finally {
+        } finally {
             System.out.println("I am gonna be here.. always!");
-        }*/
 
         }
+
+    }
 
 
 }
