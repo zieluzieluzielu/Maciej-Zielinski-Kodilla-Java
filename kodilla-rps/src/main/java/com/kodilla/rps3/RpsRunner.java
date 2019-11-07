a8b848aca00853a86c6b6aff9dbecc0f09673a8f
zieluzieluzielu <mac.ziel@gmail.com>
2019-11-07 18:20:48 +0100

    Module 7.7 RPS v3.0

--- kodilla-rps/src/main/java/com/kodilla/rps3/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps3/RpsRunner.java
@@ -1,14 +1,11 @@
 package com.kodilla.rps3;
 
-import java.util.Scanner;
 
 public class RpsRunner {
     public static void main(String[] args) {
 
         Game game = new Game();
-        game.welcomeScreen();
         game.theGame();
-        game.endGame();
 
     }
 
--- kodilla-rps/src/main/java/com/kodilla/rps3/User.java
+++ kodilla-rps/src/main/java/com/kodilla/rps3/User.java
@@ -6,14 +6,14 @@ public class User {
 
     String getUserName() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("[ RPS GAME ]\n"+
+        System.out.println("[ RPS GAME ]\n" +
                 "What is your name?");
         return scan.nextLine();
     }
 
     String getUserTurn() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please select your move(1=rock, 2=paper, 3=scissors)");
+        System.out.println("Please select your move (1=rock, 2=paper, 3=scissors)");
         return scan.nextLine();
     }
 }
