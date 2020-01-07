4b2da1f095c11e5e0861f751aa653e56723873d6
zieluzieluzielu <mac.ziel@gmail.com>
2020-01-07 20:01:32 +0100

    Module 14.3

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring.portfolio;
+
+public class Board {
+    private TaskList toDoList;
+    private TaskList inProgressList;
+    private TaskList doneList;
+
+    public Board(final TaskList toDoList, final TaskList inProgressList, final TaskList doneList) {
+        this.toDoList = toDoList;
+        this.inProgressList = inProgressList;
+        this.doneList = doneList;
+    }
+
+    public TaskList getToDoList() {
+        return toDoList;
+    }
+
+    public TaskList getInProgressList() {
+        return inProgressList;
+    }
+
+    public TaskList getDoneList() {
+        return doneList;
+    }
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,46 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BoardTestSuite {
+    @Test
+    public void testTaskExists() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+
+        //When
+        boolean toDoListExists = context.containsBean("toDoList");
+        boolean inProgressListExists = context.containsBean("inProgressList");
+        boolean doneListExists = context.containsBean("doneList");
+
+        //Then
+        Assert.assertTrue(toDoListExists);
+        Assert.assertTrue(inProgressListExists);
+        Assert.assertTrue(doneListExists);
+    }
+
+    @Test
+    public void testTaskAdd() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = (Board) context.getBean("getBoard");
+
+        //When
+        board.getToDoList().getTasks().add("toDoList");
+        board.getDoneList().getTasks().add("inProgressList");
+        board.getInProgressList().getTasks().add("doneList");
+
+        //Then
+        Assert.assertEquals(1, board.getToDoList().getTasks().size());
+        Assert.assertEquals(1, board.getInProgressList().getTasks().size());
+        Assert.assertEquals(1, board.getDoneList().getTasks().size());
+
+
+    }
+
+
+
+}
