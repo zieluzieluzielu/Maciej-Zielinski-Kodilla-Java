7b64abed9b1aa252c5bcef0a9fd8d507e0b97298
zieluzieluzielu <mac.ziel@gmail.com>
2020-01-14 11:23:10 +0100

    Module 15.2

--- kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/Board.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/Board.java
@@ -3,9 +3,9 @@ package com.kodilla.patterns.prototype;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Board {
-    final private String name;
-    final private Set<TasksList> lists = new HashSet<>();
+public class Board extends Prototype{
+    private String name;
+    private Set<TasksList> lists = new HashSet<>();
 
     public Board(String name) {
         this.name = name;
@@ -13,6 +13,10 @@ public class Board {
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public Set<TasksList> getLists() {
@@ -26,5 +30,23 @@ public class Board {
             s = s + list.toString() + "\n";
         }
         return s;
+    }
+
+    public Board shallowCopy() throws CloneNotSupportedException {
+        return (Board)super.clone();
+    }
+
+    public Board deepCopy() throws CloneNotSupportedException {
+        Board clonedBoard = (Board)super.clone();
+        clonedBoard.lists = new HashSet<>();
+        for(TasksList theList : lists) {
+            TasksList clonedList = new TasksList(theList.getName());
+            for(Task task : theList.getTasks()) {
+                clonedList.getTasks().add(task);
+            }
+            clonedBoard.getLists().add(clonedList);
+        }
+        return clonedBoard;
+
     }
 }
---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -0,0 +1,47 @@
+package com.kodilla.patterns.prototype.library;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Library extends Prototype {
+    private String name;
+    private Set<Book> books = new HashSet<>();
+
+    public Library(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public String toString() {
+        String s = "   Library books [" + name + "]";
+        for(Book book : books) {
+            s = s + "\n" + book.toString();
+        }
+        return s;
+    }
+
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return (Library)super.clone();
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = (Library)super.clone();
+        clonedLibrary.books = new HashSet<>();
+        for(Book theBooks : books) {
+            clonedLibrary.getBooks().add(theBooks);
+        }
+        return clonedLibrary;
+    }
+}
